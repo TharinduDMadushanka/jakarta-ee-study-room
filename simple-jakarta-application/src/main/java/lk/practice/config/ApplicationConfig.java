e0c2cae07151e3863074f3a90a6b275c93c0fcd3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-18 22:51:55 +0530

    complete ApplicationConfig class

--- simple-jakarta-application/src/main/java/lk/practice/config/ApplicationConfig.java
+++ simple-jakarta-application/src/main/java/lk/practice/config/ApplicationConfig.java
@@ -1,4 +1,17 @@
 package lk.practice.config;
 
-public class ApplicationConfig {
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(lk.practice.resources.ProductResource.class);
+        return resources;
+    }
 }
